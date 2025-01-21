1d920fb2fa8351935c22f7c77d88ae2a45fb0512
dezsirazvan <dezsirazvan@gmail.com>
2025-01-21 13:24:01 +0100

    add test for trackable_model?

---
+++ spec/ezlogs_ruby_agent/callbacks_tracker_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+require 'active_support/concern'
+require 'ezlogs_ruby_agent/event_queue'
+require 'active_record'
+require 'ezlogs_ruby_agent/callbacks_tracker'
+
+RSpec.describe EzlogsRubyAgent::CallbacksTracker, type: :module do
+  before do
+    schema_cache_double = double(
+      'schema_cache', 
+      columns_hash: {},
+      data_source_exists?: true
+    )
+    connection_double = double('connection', 
+      established?: true, 
+      schema_cache: schema_cache_double,
+      tables: [],
+      columns: [],
+      execute: nil
+    )
+    allow(ActiveRecord::Base).to receive(:connection).and_return(connection_double)
+
+    class DummyModel < ActiveRecord::Base
+      self.table_name = nil
+
+      include EzlogsRubyAgent::CallbacksTracker
+    end
+  end
+
+  after do
+    Object.send(:remove_const, :DummyModel)
+  end
+
+  describe '#trackable_model?' do
+    let(:dummy_model_class) { DummyModel }
+
+    it 'returns true if the model is in models_to_track' do
+      EzlogsRubyAgent.config.models_to_track = ['DummyModel']
+
+      model = dummy_model_class.new
+
+      expect(model.send(:trackable_model?)).to be true
+    end
+
+    it 'returns false if the model is in exclude_models' do
+      EzlogsRubyAgent.config.exclude_models = ['DummyModel']
+
+      model = dummy_model_class.new
+
+      expect(model.send(:trackable_model?)).to be false
+    end
+
+    it 'returns true if models_to_track is empty' do
+      EzlogsRubyAgent.config.models_to_track = []
+      EzlogsRubyAgent.config.exclude_models = []
+
+      model = dummy_model_class.new
+
+      expect(model.send(:trackable_model?)).to be true
+    end
+  end
+end
