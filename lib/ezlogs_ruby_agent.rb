bd4e3ff296b900bcb44c27e22fc835eb026f88ca
dezsirazvan <dezsirazvan@gmail.com>
2025-01-21 12:08:52 +0100

    First init of the gem

---
+++ lib/ezlogs_ruby_agent.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ezlogs_ruby_agent/version"
+
+module EzlogsRubyAgent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ezlogs_ruby_agent_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EzlogsRubyAgent do
+  it "has a version number" do
+    expect(EzlogsRubyAgent::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
