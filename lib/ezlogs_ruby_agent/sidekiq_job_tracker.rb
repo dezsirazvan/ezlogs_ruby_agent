50754c8459c395cc1db06252079a13c2ad35ac09
dezsirazvan <dezsirazvan@gmail.com>
2025-01-22 17:00:39 +0100

    refactor sidekiq job tracker

--- lib/ezlogs_ruby_agent/sidekiq_job_tracker.rb
+++ lib/ezlogs_ruby_agent/sidekiq_job_tracker.rb
@@ -5,27 +5,29 @@ module EzlogsRubyAgent
     def call(worker, job, _queue)
       config = EzlogsRubyAgent.config
       job_name = worker.class.name
-
       return yield unless trackable_job?(job_name, config)
 
       start_time = Time.current
+      correlation_id = job['correlation_id'] || SecureRandom.uuid
 
       begin
         yield
         status = 'completed'
+        error_message = nil
       rescue StandardError => e
         status = 'failed'
-        error = e.message
+        error_message = e.message
         raise e
       ensure
         end_time = Time.current
-        EzlogsRubyAgent::EventQueue.instance.add({
+        add_event({
           type: 'background_job',
           job_name: job_name,
           arguments: job['args'],
           status: status,
-          error: error,
+          error: error_message,
           duration: (end_time - start_time).to_f,
+          correlation_id: correlation_id,
           timestamp: Time.current
         })
       end
@@ -39,6 +41,10 @@ module EzlogsRubyAgent
       excluded_match = config.exclude_models.any? { |model| job_name.include?(model) }
 
       model_match && !excluded_match
+    end
+
+    def add_event(event_data)
+      EzlogsRubyAgent::EventQueue.instance.add(event_data)
     end
   end
 end
