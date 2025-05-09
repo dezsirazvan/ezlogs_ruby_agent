f32c45db37752da5a99884e34bfb59ab981d728e
dezsirazvan <dezsirazvan@gmail.com>
2025-05-09 22:09:25 +0200

    fix

--- lib/ezlogs_ruby_agent.rb
+++ lib/ezlogs_ruby_agent.rb
@@ -1,22 +1,19 @@
 require 'ezlogs_ruby_agent/railtie' if defined?(Rails)
 require 'ezlogs_ruby_agent/configuration'
-require 'ezlogs_ruby_agent/jobs/event_sender_job'
 require 'ezlogs_ruby_agent/event_writer'
 
 module EzlogsRubyAgent
-  class << self
-    def configure
-      @config ||= Configuration.new
-      yield(@config) if block_given?
-      @config
-    end
+  def self.configure
+    @config ||= Configuration.new
+    yield(@config) if block_given?
+    @config
+  end
 
-    def config
-      @config ||= Configuration.new
-    end
+  def self.config
+    @config ||= Configuration.new
+  end
 
-    def writer
-      @writer ||= EventWriter.new
-    end
+  def self.writer
+    @writer ||= EventWriter.new
   end
 end
