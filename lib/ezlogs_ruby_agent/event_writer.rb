9a0b5f05f0d8395cc43613795d1445956c8c7fc1
dezsirazvan <dezsirazvan@gmail.com>
2025-05-09 16:59:33 +0200

    add file rotation

--- lib/ezlogs_ruby_agent/event_writer.rb
+++ lib/ezlogs_ruby_agent/event_writer.rb
@@ -2,11 +2,16 @@ require 'ezlogs_ruby_agent/protobuf/event_pb'
 
 module EzlogsRubyAgent
   module EventWriter
+    LOG_FILE_PATH = 'events.log'.freeze
+    MAX_LOG_SIZE = 10 * 1024 * 1024 # 10 MB
+
     def self.write_event_to_log(event_data)
       serialized_event = serialize_event(event_data)
 
       Thread.new do
-        File.open('events.log', 'a') do |file|
+        rotate_log_file_if_needed
+
+        File.open(LOG_FILE_PATH, 'a') do |file|
           file.write(serialized_event)
         end
       rescue StandardError => e
@@ -27,6 +32,26 @@ module EzlogsRubyAgent
       )
 
       event.to_proto
+    end
+
+    def self.rotate_log_file_if_needed
+      return unless File.exist?(LOG_FILE_PATH) && File.size(LOG_FILE_PATH) > MAX_LOG_SIZE
+
+      rotate_log_files
+    end
+
+    def self.rotate_log_files
+      log_files = Dir.glob('events.log*').sort_by { |f| File.mtime(f) }
+
+      if log_files.size >= 5
+        File.delete(log_files.first) # Remove the oldest log file
+      end
+
+      return unless File.exist?(LOG_FILE_PATH)
+
+      timestamp = Time.now.strftime('%Y%m%d%H%M%S')
+      new_log_file = "events.log.#{timestamp}"
+      File.rename(LOG_FILE_PATH, new_log_file)
     end
   end
 end
